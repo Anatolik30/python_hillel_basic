dcbd150686514636f16db919f7e0b5673d66f8bc
Anatolik30 <118641718+anatolik30@users.noreply.github.com>
2023-05-11 18:11:48 +0300

    исправлен комментарий

--- lesson_2/Задание №4.py
+++ lesson_2/Задание №4.py
@@ -1,10 +1,10 @@
 consumption = float(input('Укажите расход автомобиля на 100 км: '))  # Расход топлива
-price = float(input("Стоимость 1 литра топлива?: "))                 # Цена за 1 литр
-if consumption >= 12:                                                # Комментарий
+price = float(input("Стоимость 1 литра топлива?: "))  # Цена за 1 литр
+if consumption >= 12:  # Комментарий при большом расходе топлива
     print("Дешевле будет на велосипеде!")
-distance = float(input("Укажите расстояние: "))                      # Расстояние
-if distance >= 150:                                                  # Комментарий
+distance = float(input("Укажите расстояние: "))  # Расстояние
+if distance >= 150:  # Комментарий при большом расстоянии
     print("Да, расстояние не для велосипеда!")
-consumption_at_one = consumption / 100 * distance                    # Расход топлива на 1 км
-travel_cost = consumption_at_one * price                             # Стоимость поездки
+consumption_at_one = consumption / 100 * distance  # Расход топлива на 1 км
+travel_cost = consumption_at_one * price  # Стоимость поездки
 print('Стоимость поездки: ', round(travel_cost, 2), 'грн.')
